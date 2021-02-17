6da7f7f98f992be047a63fff6c044b5387623c72
0xFeelix <34034435+0xFeelix@users.noreply.github.com>
2021-02-17 19:44:17 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*Typing script*/
+var app = document.getElementById('typing');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Developer')
+    .pauseFor(100)
+    .deleteAll()
+    .typeString('www.por')
+    .pauseFor(700)
+    .deleteChars(3)
+    .typeString('popcorn.com')
+    .pauseFor(100)
+    .start();
